1d6853dbaab032f12712e2a606d033fcc28f2441
rlatjwls <48150191+rlatjwls@users.noreply.github.com>
2019-03-02 18:36:58 +0900

    Create 20190302문제8-2.c

---
+++ 20190302문제8-2.c
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include<string.h>
+int main() {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n",a>=b );
+	printf("%d\n", a*b > 100);
+	printf("%d\n", a&b);
+}
